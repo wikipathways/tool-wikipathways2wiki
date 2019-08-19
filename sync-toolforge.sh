d9273530853c24bc62ed98a078ccbd10ed630833
Anders Riutta <git@andersriutta.com>
2019-08-18 23:11:23 -0700

    Now handling new files

--- sync-toolforge.sh
+++ sync-toolforge.sh
@@ -1,3 +1,4 @@
-rsync -rIl --delete --exclude "./*" --include "./{svg2commons}/*.{py,json}" tools-login.wmflabs.org:~/tool-wikipathways2wiki/
+#rsync -rIl --delete --exclude "./*" --include "./{svg2commons,gpml2svg}/*.{py,json}" tools-login.wmflabs.org:~/tool-wikipathways2wiki/
+rsync -rRIl --delete ./{gpml2svg,svg2commons}/*.{py,json} tools-login.wmflabs.org:~/tool-wikipathways2wiki/
 
-ssh tools-login.wmflabs.org 'rsync -rIl --delete --exclude "./*" --include "tool-wikipathways2wiki/{svg2commons}/*.{py,json}" ./tool-wikipathways2wiki/ /data/project/wikipathways2wiki/tool-wikipathways2wiki-tmp/; become wikipathways2wiki "/data/project/wikipathways2wiki/update.sh"; rm -rf /data/project/wikipathways2wiki/tool-wikipathways2wiki-tmp'
+ssh tools-login.wmflabs.org 'cd tool-wikipathways2wiki; rsync -rRIl --delete ./{svg2commons,gpml2svg}/*.{py,json} /data/project/wikipathways2wiki/tool-wikipathways2wiki-tmp/; become wikipathways2wiki "/data/project/wikipathways2wiki/update.sh"; rm -rf /data/project/wikipathways2wiki/tool-wikipathways2wiki-tmp'
