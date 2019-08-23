0c4c7247b7f488f6afe8362b21d1d8bb14b7807e
Anders Riutta <git@andersriutta.com>
2019-08-22 22:08:11 -0700

    Squashed 'mynixpkgs/' content from commit 117e6e4
    git-subtree-dir: mynixpkgs
    git-subtree-split: 117e6e4b55c2b70ae0c5e23564c31a88954cba3b

---
+++ environments/python.nix
@@ -0,0 +1,20 @@
+# See README.md for instructions on installing/updating.
+
+with import <nixpkgs> { config.allowUnfree = true; };
+let
+  common = import ./common.nix;
+  custom = import ../all-custom.nix;
+in common ++ [
+
+  pkgs.python3
+  custom.black
+
+] ++ (if stdenv.isDarwin then [
+
+  # XCode Command Line Tools
+  # TODO: do we need cctools?
+  #pkgs.darwin.cctools
+
+] else [
+
+])
