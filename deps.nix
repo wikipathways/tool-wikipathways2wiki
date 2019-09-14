ff3bfa1f1d65b847dd5b57974978203b50908c07
Anders Riutta <git@andersriutta.com>
2019-09-13 17:34:59 -0700

    Add svgo

--- deps.nix
+++ deps.nix
@@ -27,10 +27,9 @@ in
     custom.bridgedb
     custom.gpml2pvjson
     custom.pvjs
+    custom.svgo
 
     pkgs.coreutils
-    pkgs.xmlstarlet
-    pkgs.bc
   ] ++ (if stdenv.isDarwin then [
 
   ] else [
